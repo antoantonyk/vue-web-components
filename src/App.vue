d6d0bb2ba32f17e3ae3ead4f0512a31523bc6b1a
antoantonyk <antoantony8@gmail.com>
2019-11-15 01:02:42 +0530

    feat: components added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ul>
+      <TreeItem class="item" :item="treeData" />
+    </ul>
+    <BookList :books="books" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import TreeItem from "./components/TreeItem.vue";
+import BookList from "./components/BookList.vue";
 
 export default {
   name: "app",
+  data: function() {
+    return {
+      treeData: {
+        name: "My Tree",
+        children: [
+          { name: "hello" },
+          { name: "wat" },
+          {
+            name: "child folder",
+            children: [
+              {
+                name: "child folder",
+                children: [{ name: "hello" }, { name: "wat" }]
+              },
+              { name: "hello" },
+              { name: "wat" },
+              {
+                name: "child folder",
+                children: [{ name: "hello" }, { name: "wat" }]
+              }
+            ]
+          }
+        ]
+      },
+      books: [
+        { id: 1, title: "Bla", author: "blabla" },
+        { id: 2, title: "Bla", author: "blabla" },
+        { id: 3, title: "Bla", author: "blabla" },
+        { id: 4, title: "Bla", author: "blabla" },
+        { id: 5, title: "Bla", author: "blabla" }
+      ]
+    };
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    TreeItem,
+    BookList
   }
 };
 </script>
---
+++ src/components/BookList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class>
+    <li v-for="book in books" :key="book.id">
+      <span class="title">{{ book.title }}</span> &mdash;
+      <span class="author">{{ book.author }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "BookList",
+  props: ["books"]
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/_common.scss";
+</style>
